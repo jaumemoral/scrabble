863e6aca0f2bd6b5b6aae8443c216333a0cf7179
Jaume Moral <jaumem@fib.upc.edu>
2016-06-27 10:58:46 +0200

    Solution without ensuring number of tiles. Test 3 fails

--- src/main/java/scrabble/Scrabble.java
+++ src/main/java/scrabble/Scrabble.java
@@ -1,35 +1,102 @@
+package scrabble;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Scrabble {
 
-  Map<Integer,Set<Char>> tilesIndexedByFrequency=new HashMap<Integer,Set<Char>>
-  Map<Char,Integer> frequenciesIndexedByTile=new HashMap<Char,Integer>
+	
+	Map<Character, Integer> frequenciesIndexedByTile = new HashMap<Character, Integer>();
 
-  Scrabble (String tiles) {
-    for (char tile:tiles) addTile(tile);
-  }
+	Scrabble() {
+		this("AAAAAAAAA" + "BB" + "CC" + "DDDD" + "EEEEEEEEEEEE" + "FF" + "GGG" + "HH" + "IIIIIIIII" + "J" + "K"
+				+ "LLLL" + "MM" + "NNNNNN" + "OOOOOOOO" + "PP" + "Q" + "RRRRRR" + "SSSS" + "TTTTTT" + "UUUU" + "VV"
+				+ "WW" + "X" + "YY" + "Z" + "__");
+	}
 
-  public void addTile(char tile) {
-    int frequency=0;
-    try {
-	frequency=frequenciesIndexedByTile.get(tile)
-    } catch (Exception e) {
-        frequenciesIndexedByTile.put(tile,0)
-    }
-    frequenciesIndexedByTile.put(char,frequency++)
-    try {
-	Set s=tilesIndexedByFrequency.get(frequency);
-    } catch (Exception e) {
-        tilesIndexedByFrequency.put(frequency,new TreeSet<Char>())
-    }
-    s.add(tile);
-  }
+	Scrabble(String tiles) {
+		for (char tile : tiles.toCharArray())
+			addTile(tile);
+	}
 
-  public void removeTile(char tile) {
-    int frequency=frequenciesIndexedByTile(tile);
-    frequenciesIndexedByTile.put(tile,frequency-1);
-    tilesIndexedByFrequency.remove(frequency,tile);
-    tilesIndexedByFrequency.add(frequency-1,tile);
-  }
+	public void addTile(char tile) {
+		int frequency = 0;
+		try {
+			frequency = frequenciesIndexedByTile.get(tile);
+		} catch (Exception e) {
+			frequenciesIndexedByTile.put(tile, 0);
+		}
+		frequency++;
+		frequenciesIndexedByTile.put(tile, frequency);
+	}
+	
+	SortedMap<Frequency, Set<Character>> indexTilesByFrequency() {
+		SortedMap<Frequency, Set<Character>> tilesIndexedByFrequency = new TreeMap<Frequency, Set<Character>>();
+		for (Map.Entry<Character, Integer> pair : frequenciesIndexedByTile.entrySet()) {
+			Character tile=pair.getKey();
+			Integer frequency=pair.getValue();
+			Set<Character> s=tilesIndexedByFrequency.get(new Frequency(frequency));
+			if (s==null) {
+				s=new TreeSet<Character>();
+			    tilesIndexedByFrequency.put(new Frequency(frequency),s);
+			} 
+			s.add(tile);
+		}
+		return tilesIndexedByFrequency;
+	}
 
-  public void toString();
+	public void removeTile(char tile) {
+		int frequency = frequenciesIndexedByTile.get(tile);
+		frequenciesIndexedByTile.put(tile, frequency - 1);
+	}
+
+	public void removeTiles(String tiles) {
+		for (char tile : tiles.toCharArray())
+			removeTile(tile);
+	}
+
+	public String toString() {
+		SortedMap<Frequency, Set<Character>> tilesIndexedByFrequency = indexTilesByFrequency();
+		StringBuffer result = new StringBuffer();		
+		for (Map.Entry<Frequency, Set<Character>> pair : tilesIndexedByFrequency.entrySet()) {
+			result.append(pair.getKey());
+			result.append(": ");
+			boolean firstTime=true;
+			for (Character tile : pair.getValue()) {
+				if (!firstTime) result.append(", "); 
+				result.append(tile);
+				firstTime=false;
+			}
+			result.append("\n");
+		}
+		System.out.println(result);
+		return result.toString();
+	}
 }
 
+class Frequency implements Comparable<Frequency>{
+	
+	Integer value;
+	
+	Frequency(int value) {
+		this.value=value;
+	}
+	
+	public int getValue() {
+		return this.value;
+	}	
+	
+	// Return opposite value
+	public int compareTo(Frequency f) {
+		return -value.compareTo(f.getValue());
+	}
+	
+	public String toString() {
+		return value.toString();
+	}
+
+}
---
+++ src/test/java/scrabble/TestScrabble.java
@@ -0,0 +1,63 @@
+package scrabble;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestScrabble {
+
+	@Test
+	public void testInitial() {
+		Scrabble s=new Scrabble();
+		s.removeTiles("");
+		assertEquals(
+				"12: E\n"+
+				"9: A, I\n"+
+				"8: O\n"+
+				"6: N, R, T\n"+
+				"4: D, L, S, U\n"+
+				"3: G\n"+
+				"2: B, C, F, H, M, P, V, W, Y, _\n"+
+				"1: J, K, Q, X, Z\n",s.toString());
+	}
+
+	@Test
+	public void test1() {
+		Scrabble s=new Scrabble();
+		s.removeTiles("AEERTYOXMCNB_S");
+		assertEquals(
+				"10: E\n"+
+				"9: I\n"+
+				"8: A\n"+
+				"7: O\n"+
+				"5: N, R, T\n"+
+				"4: D, L, U\n"+
+				"3: G, S\n"+
+				"2: F, H, P, V, W\n"+
+				"1: B, C, J, K, M, Q, Y, Z, _\n"+
+				"0: X\n",s.toString());
+	}
+
+	@Test
+	public void test2() {
+		Scrabble s=new Scrabble();
+		s.removeTiles("LQTOONOEFFJZT");
+		assertEquals(
+				"11: E\n"+
+				"9: A, I\n"+
+				"6: R\n"+
+				"5: N, O\n"+
+				"4: D, S, T, U\n"+
+				"3: G, L\n"+
+				"2: B, C, H, M, P, V, W, Y, _\n"+
+				"1: K, X\n"+
+				"0: F, J, Q, Z\n",s.toString());
+	}
+	
+	@Test
+	public void test3() {
+		Scrabble s=new Scrabble();
+		s.removeTiles("AXHDRUIOR_XHJZUQEE");
+		assertEquals("Invalid input. More X's have been taken from the bag than possible.",s.toString());
+	}	
+}
